c02d755a459f94fd3acfe212be814b22957b128b
Thomas Swokowski <tom@Thomass-Mac-mini.local>
2023-06-17 17:39:28 -0500

    WEB-001: Implement show/hide nav on scroll

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState, useRef } from 'react';
 import { useRouter } from 'next/router';
 import Link from 'next/link';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -6,11 +7,41 @@ import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 const Navigation = () => {
   const router = useRouter();
+  const [isHidden, setIsHidden] = useState(false);
+  const [prevScrollPos, setPrevScrollPos] = useState(0);
+  const navHeight = 112; // the original height of the nav
+  const navRef = useRef<HTMLDivElement>(null);
 
   const isActive = (pathname: string) => router.pathname === pathname;
 
+  const checkScroll = () => {
+    const currentScrollPos = window.pageYOffset;
+    const visible =
+      currentScrollPos < navHeight || prevScrollPos > currentScrollPos;
+    setIsHidden(!visible);
+    setPrevScrollPos(currentScrollPos);
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', checkScroll);
+    return () => window.removeEventListener('scroll', checkScroll);
+  }, [prevScrollPos]);
+
+  useEffect(() => {
+    if (navRef.current) {
+      navRef.current.style.top = isHidden ? `-${navHeight}px` : '0';
+      navRef.current.style.position =
+        prevScrollPos > navHeight ? 'fixed' : 'relative';
+    }
+  }, [prevScrollPos, isHidden]);
+
   return (
-    <div className="border-b-2">
+    <div
+      ref={navRef}
+      className={`border-b-2 bg-white w-full z-10 transition-all duration-300 ${
+        prevScrollPos > navHeight ? 'fixed' : 'relative'
+      }`}
+    >
       <div className="flex justify-between items-center text-primary p-4 pb-4">
         <div>
           <h1 className="text-base font-semibold">Tom Swokowski's Blog</h1>
