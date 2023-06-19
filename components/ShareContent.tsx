d7471c3690e6a1a7619949e56ed27be033d6d36a
Thomas Swokowski <tom@Thomass-Mac-mini.local>
2023-06-19 17:14:27 -0500

    WEB-008: Continue working on the share content modal

--- components/ShareContent.tsx
+++ components/ShareContent.tsx
@@ -1,4 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
+import {
+  EmailIcon,
+  EmailShareButton,
+  LinkedinIcon,
+  LinkedinShareButton,
+  TwitterIcon,
+  TwitterShareButton,
+} from 'react-share';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
 
 type ShareContentProps = {
   url: string;
@@ -6,12 +15,19 @@ type ShareContentProps = {
 };
 
 const ShareContent: React.FC<ShareContentProps> = ({ url, onClose }) => {
+  const [copySuccess, setCopySuccess] = useState('');
+
+  const stopPropagation = (e: React.MouseEvent) => {
+    e.stopPropagation();
+  };
+
   return (
     <div
-      className="fixed z-10 inset-0 overflow-y-auto"
+      className="fixed inset-0 z-10 overflow-y-auto"
       aria-labelledby="modal-title"
       role="dialog"
       aria-modal="true"
+      onClick={onClose}
     >
       <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
         <div
@@ -25,36 +41,37 @@ const ShareContent: React.FC<ShareContentProps> = ({ url, onClose }) => {
           &#8203;
         </span>
         <div
-          className="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl 
-                transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full"
+          className="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full"
+          onClick={stopPropagation}
         >
           <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
             <div className="sm:flex sm:items-start">
               <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
-                <h3
-                  className="text-lg leading-6 font-medium text-gray-900"
-                  id="modal-title"
-                >
-                  Share Content
-                </h3>
-                <div className="mt-2">
-                  <p className="text-sm text-gray-500">
-                    Copy the following URL to share this content:
-                  </p>
-                  <p className="text-sm text-blue-500">{url}</p>
+                <h3 className="text-2xl mb-4">Share this content</h3>
+                <div className="flex justify-center w-3/4 mb-4">
+                  <EmailShareButton url={url} className="mx-2">
+                    <EmailIcon size={32} round />
+                  </EmailShareButton>
+                  <LinkedinShareButton url={url} className="mx-2">
+                    <LinkedinIcon size={32} round />
+                  </LinkedinShareButton>
+                  <TwitterShareButton url={url} className="mx-2">
+                    <TwitterIcon size={32} round />
+                  </TwitterShareButton>
                 </div>
+                <CopyToClipboard
+                  text={url}
+                  onCopy={() => setCopySuccess('URL copied to clipboard.')}
+                >
+                  <button className="w-40 py-2 text-center text-white rounded-md shadow-md bg-primary">
+                    Copy to Clipboard
+                  </button>
+                </CopyToClipboard>
+                {copySuccess && (
+                  <span className="mt-2 text-green-500">{copySuccess}</span>
+                )}
               </div>
             </div>
-          </div>
-          <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
-            <button
-              type="button"
-              className="mt-3 w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 
-                bg-blue-600 text-base font-medium text-white hover:bg-blue-700 focus:outline-none sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
-              onClick={onClose}
-            >
-              Close
-            </button>
           </div>
         </div>
       </div>
