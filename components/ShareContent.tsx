c13be1d040cdb09e36544b449cbaa31cdd9a8e76
Thomas Swokowski <tom@Thomass-Mac-mini.local>
2023-06-19 16:39:04 -0500

    WEB-008: Start implementing sharecontent modal

--- components/ShareContent.tsx
+++ components/ShareContent.tsx
@@ -1,7 +1,65 @@
-import { useState } from 'react';
+import React from 'react';
 
-const ShareContent: React.FC = () => {
-  return <div>{/* Share Content Component */}</div>;
+type ShareContentProps = {
+  url: string;
+  onClose: () => void;
+};
+
+const ShareContent: React.FC<ShareContentProps> = ({ url, onClose }) => {
+  return (
+    <div
+      className="fixed z-10 inset-0 overflow-y-auto"
+      aria-labelledby="modal-title"
+      role="dialog"
+      aria-modal="true"
+    >
+      <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+        <div
+          className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"
+          aria-hidden="true"
+        ></div>
+        <span
+          className="hidden sm:inline-block sm:align-middle sm:h-screen"
+          aria-hidden="true"
+        >
+          &#8203;
+        </span>
+        <div
+          className="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl 
+                transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full"
+        >
+          <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+            <div className="sm:flex sm:items-start">
+              <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+                <h3
+                  className="text-lg leading-6 font-medium text-gray-900"
+                  id="modal-title"
+                >
+                  Share Content
+                </h3>
+                <div className="mt-2">
+                  <p className="text-sm text-gray-500">
+                    Copy the following URL to share this content:
+                  </p>
+                  <p className="text-sm text-blue-500">{url}</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
+            <button
+              type="button"
+              className="mt-3 w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 
+                bg-blue-600 text-base font-medium text-white hover:bg-blue-700 focus:outline-none sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
+              onClick={onClose}
+            >
+              Close
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ShareContent;
